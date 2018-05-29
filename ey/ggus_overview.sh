04fd6162fed1626c871435f23a814aaa15675885
Ron Trompert <ron.trompert@surfsara.nl>
2018-05-29 07:36:43 +0200

    date and timestrings added

--- ey/ggus_overview.sh
+++ ey/ggus_overview.sh
@@ -5,6 +5,9 @@ echo "my_hdr From: EY <>"
 echo "my_hdr Sender: EY <>"
 } > ~/.muttrc
 
+datestring=`env LC_TIME=C date --date="15 days ago" '+%B %Y'`
+timestring=`env LC_TIME=C date '+%F'`
+
 address=""
 
 tmpfile=`mktemp`
@@ -14,13 +17,13 @@ echo >>${tmpfile}
 /usr/local/bin/gorilla.py username password -l NIKHEF-ELPROD open 1>>${tmpfile} 2>/dev/null
 echo >>${tmpfile}
 
-/usr/bin/mutt -s "Open GGUS tickets overview" $address <${tmpfile}
+/usr/bin/mutt -s "Open GGUS tickets overview "$timestring $address <${tmpfile}
 username
 /usr/local/bin/gorilla.py username password -l SARA-MATRIX terminal 1>${tmpfile} 2>/dev/null
 echo >>${tmpfile}
 /usr/local/bin/gorilla.py username password -l NIKHEF-ELPROD terminal 1>>${tmpfile} 2>/dev/null
 echo >>${tmpfile}
 
-/usr/bin/mutt -s "Overview GGUS tickets that were closed last month" $address <${tmpfile}
+/usr/bin/mutt -s "Overview GGUS tickets that were closed in "$datestring $address <${tmpfile}
 
 rm -f ${tmpfile}
